09a5f576d17e499b8785e28c6aa4d85fc8835952
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-02-02 12:40:11 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Loading from './components/Loading'
 import Navbar from './components/Navbar'
+import Notification from './components/Notification'
+import Login from './components/user/Login'
 
 function App() {
   return (
+    <>
+    <Loading/>
+    <Notification/>
   <Navbar/>
+  <Login/>
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import {createRoot} from 'react-dom/client'
-import App from './App'
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
 //Roboto Font
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import ContextProvider from "./Context/ContextProvider";
 
-
-
-createRoot(document.getElementById('root')).render(
-    <App/>
-)
+createRoot(document.getElementById("root")).render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+);
