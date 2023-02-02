00829705bd04aef45631777d8f10999f8d1e2b8d
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-02-02 12:40:03 +0530

    created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,59 @@
-import { AppBar, Container, Toolbar } from '@mui/material'
-import React from 'react'
+import { Lock, Menu } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { useValue } from "../Context/ContextProvider";
+import UserIcons from "./user/UserIcons";
+
 
 function Navbar() {
+  const {
+    state: { currentUser },
+    dispatch
+  } = useValue();
   return (
-    <AppBar maxWidth='lg'>
-        <Container>
-            <Toolbar disableGutters></Toolbar>
-        </Container>
+    <AppBar maxwidth="lg">
+      <Container>
+        <Toolbar disableGutters>
+          <Box sx={{ mx: 1 }}>
+            <IconButton size="large" color="inherit">
+              <Menu />
+            </IconButton>
+          </Box>
+          <Typography
+            variant="h6"
+            component="h1"
+            noWrap
+            sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+          >
+            You Are Welcome
+          </Typography>
+          <Typography
+            variant="h6"
+            component="h1"
+            noWrap
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+          >
+            Welcome..!
+          </Typography>
+          {!currentUser ? (
+            <Button color="inherit" startIcon={<Lock />} onClick={()=>{dispatch({type:'OPEN_LOGIN'})}}>
+              Login
+            </Button>
+          ) : (
+            <UserIcons />
+          )}
+        </Toolbar>
+      </Container>
     </AppBar>
-    )
+  );
 }
 
-export default Navbar
+export default Navbar;
---
+++ src/components/user/PasswordField.js
@@ -0,0 +1,38 @@
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { IconButton, InputAdornment, TextField } from "@mui/material";
+import React, { useState } from "react";
+
+function PasswordField({ passwordRef, id = "password", label = "Password" }) {
+  const [showPassword, setShowPasword] = useState(false);
+
+  const handleClick = () => {
+    setShowPasword(!showPassword);
+  };
+  const handleMouseDown = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <TextField
+      margin="normal"
+      variant="standard"
+      id={id}
+      label={label}
+      type={showPassword ? "text" : "password"}
+      fullWidth
+      inputRef={passwordRef}
+      inputProps={{ minLength: 8 }}
+      required
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton onClick={handleClick} onMouseDown={handleMouseDown}>
+              {showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+}
+
+export default PasswordField;
