1c828361cc5d3fbdbfb786a5448e4e5eaaebf375
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-02-05 17:30:27 +0530

    new component added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-
 //Roboto Font
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import ContextProvider from "./Context/ContextProvider";
+import ContextProvider from "./context/ContextProvider";
 
 createRoot(document.getElementById("root")).render(
   <ContextProvider>
