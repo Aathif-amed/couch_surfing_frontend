938a3e8541db42cedec30c8dab14e586e3625b4a
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-02-07 09:24:20 +0530

    bottom nav component imported

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import Loading from './components/Loading'
-import Navbar from './components/Navbar'
-import Notification from './components/Notification'
-import Login from './components/user/Login'
+import React from "react";
+import Loading from "./components/Loading";
+import Navbar from "./components/Navbar";
+import Notification from "./components/Notification";
+import BottomNav from "./components/BottomNav";
+import Login from "./components/user/Login";
 
 function App() {
   return (
     <>
-    <Loading/>
-    <Notification/>
-  <Navbar/>
-  <Login/>
+      <Loading />
+      <Notification />
+      <Navbar />
+      <Login />
+      <BottomNav />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
