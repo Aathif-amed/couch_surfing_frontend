b35b7b16dfdd1b20b145368237af8c77c0781244
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-02-11 16:09:12 +0530

    dashboard menu added

--- src/components/user/UserMenu.js
+++ src/components/user/UserMenu.js
@@ -1,6 +1,7 @@
-import { Logout, Settings } from "@mui/icons-material";
+import { Dashboard, Logout, Settings } from "@mui/icons-material";
 import { ListItemIcon, Menu, MenuItem } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useValue } from "../../context/ContextProvider";
 import CheckGUserToken from "../../hooks/CheckGUserToken";
 import Profile from "./Profile";
@@ -14,36 +15,53 @@ function UserMenu({ anchorUserMenu, setAnchorUserMenu }) {
   const handleCloseUserMenu = () => {
     setAnchorUserMenu(null);
   };
- 
+  const navigate = useNavigate();
   return (
     <>
-    <Menu
-      anchorEl={anchorUserMenu}
-      open={Boolean(anchorUserMenu)}
-      onClose={handleCloseUserMenu}
-      onClick={handleCloseUserMenu}
+      <Menu
+        anchorEl={anchorUserMenu}
+        open={Boolean(anchorUserMenu)}
+        onClose={handleCloseUserMenu}
+        onClick={handleCloseUserMenu}
       >
-        {!currentUser.google&&
-      <MenuItem onClick={()=>{dispatch({type:'UPDATE_PROFILE',payload:{open:true,file:null,photoURL:currentUser?.photoURL}})}}>
-        <ListItemIcon>
-          <Settings fontSize="small" />
-        </ListItemIcon>
-        Profile
-      </MenuItem>
-         }
-      <MenuItem
-        onClick={() => {
-          dispatch({ type: "UPDATE_USER", payload: null });
-        }}
-      >
-        <ListItemIcon>
-          <Logout fontSize="small" />
-        </ListItemIcon>
-        Logout
-      </MenuItem>
-    </Menu>
-    <Profile/>
-      </>
+        {!currentUser.google && (
+          <MenuItem
+            onClick={() => {
+              dispatch({
+                type: "UPDATE_PROFILE",
+                payload: {
+                  open: true,
+                  file: null,
+                  photoURL: currentUser?.photoURL,
+                },
+              });
+            }}
+          >
+            <ListItemIcon>
+              <Settings fontSize="small" />
+            </ListItemIcon>
+            Profile
+          </MenuItem>
+        )}
+        <MenuItem onClick={() => navigate("/dashboard")}>
+          <ListItemIcon>
+            <Dashboard fontSize="small" />
+          </ListItemIcon>
+          Dashboard
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            dispatch({ type: "UPDATE_USER", payload: null });
+          }}
+        >
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+      <Profile />
+    </>
   );
 }
 
