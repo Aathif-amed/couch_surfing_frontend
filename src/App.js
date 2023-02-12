3e14b54be1992bec9cb81c5c570c98fd889b121e
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-02-12 19:19:34 +0530

    components added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Loading from "./components/Loading";
+import Notification from "./components/Notification";
+import Room from "./components/rooms/Room";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
+      <Loading />
+      <Notification />
       <BrowserRouter>
         <Routes>
           <Route path="dashboard/*" element={<Dashboard />} />
           <Route path="*" element={<Home />} />
         </Routes>
       </BrowserRouter>
+      <Room />
     </>
   );
 }
