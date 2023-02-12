d0f155affa99cdd4c8769d358a048e2e36fd4988
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-02-12 19:22:54 +0530

    components removed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,14 @@
 import React from "react";
-import Loading from "../components/Loading";
 import Navbar from "../components/Navbar";
-import Notification from "../components/Notification";
 import BottomNav from "../components/BottomNav";
 import Login from "../components/user/Login";
-import Room from "../components/rooms/Room";
 
 function Home() {
   return (
     <>
-      <Loading />
-      <Notification />
       <Login />
       <Navbar />
       <BottomNav />
-      <Room />
     </>
   );
 }
