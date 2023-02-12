b2ea0097a4dee13b6a043b786613cd7840aa7e89
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-02-12 13:24:48 +0530

    users component for dashboard

--- src/pages/dashboard/users/Users.js
+++ src/pages/dashboard/users/Users.js
@@ -1,10 +1,105 @@
-import React, { useEffect } from "react";
+import { Avatar, Box, Typography } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import { DataGrid, gridClasses } from "@mui/x-data-grid";
+import moment from "moment";
+import React, { useEffect, useMemo, useState } from "react";
+import { getUsers } from "../../../actions/user";
+import { useValue } from "../../../context/ContextProvider";
+import UsersActions from "./UsersActions";
 
 function Users({ setSelectedLink, link }) {
+  const {
+    state: { users },
+    dispatch,
+  } = useValue();
+
+  const [pageSize, setPageSize] = useState(5);
+  const [rowId, setRowId] = useState(null);
+  //useeffect to getUsers if there is no user
   useEffect(() => {
     setSelectedLink(link);
+    if (users.length === 0) {
+      getUsers(dispatch);
+    }
   }, []);
-  return <div>Users</div>;
+
+  const columns = useMemo(
+    () => [
+      {
+        field: "photoURL",
+        headerName: "Avatar",
+        width: 60,
+        renderCell: (params) => <Avatar src={params.row.photoURL} />,
+        sortable: false,
+        filterable: false,
+      },
+      { field: "fName", headerName: "First Name", width: 170 },
+      { field: "lName", headerName: "Last Name", width: 170 },
+      { field: "email", headerName: "Email", width: 200 },
+      {
+        field: "role",
+        headerName: "Role",
+        width: 100,
+        type: "singleSelect",
+        valueOptions: ["basic", "editor", "admin"],
+        editable: true,
+      },
+      {
+        field: "active",
+        headerName: "Active",
+        width: 100,
+        type: "boolean",
+        editable: true,
+      },
+      {
+        field: "createdAt",
+        headerName: "Created At",
+        width: 200,
+        renderCell: (params) =>
+          moment(params.row.createdAt).format("YYYY-MM-DD HH:MM:SS"),
+      },
+      { field: "_id", headerName: "Id", width: 220 },
+      {
+        field: "actions",
+        headerName: "Actions",
+        width: 220,
+        renderCell: (params) => (
+          <UsersActions {...{ params, rowId, setRowId }} />
+        ),
+      },
+    ],
+    [rowId]
+  );
+  return (
+    <Box sx={{ height: 400, width: "100%" }}>
+      <Typography
+        variant="h3"
+        component="h3"
+        sx={{ textAlign: "center", mt: 3, mb: 3 }}
+      >
+        Manage Users
+      </Typography>
+      <DataGrid
+        columns={columns}
+        rows={users}
+        getRowId={(row) => row._id}
+        rowsPerPageOptions={[5, 15, 30]}
+        pageSize={pageSize}
+        onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+        getRowSpacing={(params) => ({
+          top: params.isFirstVisible ? 0 : 5,
+          bottom: params.isLastVisible ? 0 : 5,
+        })}
+        sx={{
+          [`& .${gridClasses.row}`]: {
+            bgcolor: (theme) =>
+              theme.palette.mode === "light" ? grey[200] : grey[900],
+          },
+        }}
+        onCellEditCommit={(params) => setRowId(params.id)}
+      />
+    </Box>
+  );
 }
 
 export default Users;
